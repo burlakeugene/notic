f90b316442b6403cbfe3c9c4f752a9e6ed90c391
Eugene Burlak <burlakeugene@gmail.com>
2020-10-08 16:10:16 +0300

    udp

--- src/Notic/index.js
+++ src/Notic/index.js
@@ -5,6 +5,10 @@ export default class Notic {
     this.types = {
       default: 'info',
       list: {
+        loading: {
+          icon:
+            '<svg viewBox="0 0 100 100"><path d="M10,50 a1,1 0 0,0 80,0" /></svg>',
+        },
         success: {
           icon:
             '<svg viewBox="0 0 512 512"><path d="M387.106,167.497c-5.857-5.857-15.354-5.858-21.212,0.001L220.705,312.685l-74.599-74.597 c-5.857-5.857-15.355-5.858-21.213,0.001c-5.857,5.858-5.857,15.355,0,21.213l85.205,85.203c2.929,2.929,6.768,4.393,10.606,4.393 c3.839,0,7.678-1.464,10.606-4.394L387.106,188.71C392.964,182.853,392.964,173.355,387.106,167.497z"/><path d="M437.019,74.981C388.667,26.629,324.379,0,256,0C187.62,0,123.333,26.629,74.981,74.981C26.628,123.334,0,187.621,0,256 c0,68.381,26.628,132.668,74.981,181.02C123.332,485.372,187.62,512,256,512s132.667-26.628,181.019-74.98 C485.371,388.668,512,324.381,512,256C512,187.621,485.371,123.334,437.019,74.981z M256,482C131.383,482,30,380.617,30,256 S131.383,30,256,30s226,101.383,226,226S380.617,482,256,482z"/></svg>',
@@ -97,7 +101,7 @@ export default class Notic {
     message.dom.classList.remove('notic-message__visible');
     message.dom.style.height = 0;
     message.dom.ontransitionend = () => {
-      if (message.dom.style.height === '0px'){
+      if (message.dom.style.height === '0px') {
         if (!message.visible) return;
         message.visible = false;
         this.removeMessage(message);
@@ -111,5 +115,31 @@ export default class Notic {
       });
     messages.list.splice(index, 1);
     message.dom.parentNode.removeChild(message.dom);
+  }
+  loadingOn() {
+    let { types } = this;
+    if (this.state.loading) return;
+    this.loadingDom = document.createElement('div');
+    this.loadingDom.classList.add('notic-loading');
+    this.loadingDom.innerHTML = types.list['loading'].icon;
+    this.rootDOM.appendChild(this.loadingDom);
+    this.state.loading = true;
+    this.rootDOM.classList.add('notic__loading');
+    this.rootDOM.ontransitionend = () => {
+      this.loadingDom.classList.add('notic-loading__show');
+    };
+  }
+  loadingOff() {
+    this.state.loading = false;
+    this.loadingDom.classList.remove('notic-loading__show');
+    this.loadingDom.ontransitionend = () => {
+      this.rootDOM.classList.remove('notic__loading');
+    };
+    this.rootDOM.ontransitionend = () => {
+      this.loadingDom && this.loadingDom.parentNode.removeChild(this.loadingDom);
+    };
+  }
+  clear() {
+    
   }
 }
