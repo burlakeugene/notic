9ec9339b54de19c589c6961548569af79a93b539
Eugene Burlak <burlakeugene@gmail.com>
2020-11-24 16:32:54 +0300

    upd

--- src/Notic/index.js
+++ src/Notic/index.js
@@ -23,7 +23,7 @@ export default class Notic {
       list: {
         loading: {
           icon:
-            '<svg viewBox="0 0 100 100"><path d="M10,50 a1,1 0 0,0 80,0" /></svg>',
+            '<svg version="1.1"  xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="25 25 50 50"><circle cx="50" cy="50" r="20" fill="none" stroke-linecap="round" stroke-dashoffset="0" stroke-dasharray="100, 200"><animateTransform attributeName="transform" attributeType="XML" type="rotate" from="0 50 50" to="360 50 50" dur="2.5s" repeatCount="indefinite"/><animate attributeName="stroke-dashoffset" values="0;-30;-124" dur="1.25s" repeatCount="indefinite"/><animate attributeName="stroke-dasharray" values="0,200;110,200;110,200" dur="1.25s" repeatCount="indefinite"/></circle></svg>',
         },
         success: {
           icon:
